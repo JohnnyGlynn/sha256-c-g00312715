6e0d4916b473e636e427cd3ee867732d9c44517d
JohnnyGlynn <idyslexiahave@gmail.com>
2021-02-26 00:04:14 +0000

    Initial Path input and printing first char  of opened file

---
+++ sha256.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main() {
+    FILE *file;
+    char filepath[255], out;
+
+    printf("Enter a file path: ");
+    scanf("%s", &filepath);
+
+    file = fopen(&filepath, "r");
+    if (file == NULL) 
+    { 
+        printf("Cannot open file \n"); 
+        return 0;
+    } 
+
+    printf("Ive opened a file\n");
+    fscanf(file,"%c", &out);
+
+    printf("Value of %c \n", out);
+
+    fclose(file); 
+
+    return 0;
+}
